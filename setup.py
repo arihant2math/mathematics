c153fee3fb839dcef9a143ec7fa98c3ada92b9b7
Ashwin <arihant2math@gmail.com>
2021-01-22 09:33:29 -0800

    built 0.0.0a3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mathematics-package",
-    version="0.0.0a2",
+    version="0.0.0a3",
     author="Ashwin Naren",
     author_email="arihant2math@gmail.com",
     description="A mathematics package",
