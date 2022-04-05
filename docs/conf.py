f12e9de21b7dbbc511d401e618fcba14bf16ce49
Ashwin Naren <arihant2math@gmail.com>
2022-04-05 08:47:47 -0700

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,14 @@ version = '0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.duration']
+extensions = [
+	"sphinx.ext.autodoc",
+	"sphinx.ext.intersphinx",
+	"sphinxcontrib.log_cabinet",
+	"pallets_sphinx_themes",
+	"sphinx_issues",
+	"sphinx_tabs.tabs"
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
