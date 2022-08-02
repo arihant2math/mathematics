2ac86bf317b5cdee26a4db1a18656233d106fdcd
Ashwin <arihant2math@gmail.com>
2022-08-01 20:27:57 -0700

    algebra

--- mathematics/algebra/variable.py
+++ mathematics/algebra/variable.py
@@ -1,39 +1,16 @@
+from mathematics.algebra.term import Term
+
+
 class Variable:
     """Emulates a Variable"""
 
-    def __init__(self, var):
-        self.name = ""
-        self.coefficient = 1
-        for char in var:
-            if char.isalpha():
-                if self.name != "":
-                    raise ValueError("Invalid variable name, must be one letter")
-                self.name = char
-
-        if var - self.name == "":
-            self.coefficient = 1
-        else:
-            self.coefficient = int(var - self.name)
-
-    def __str__(self):
-        if self.coefficient == 1:
-            return self.name
-        else:
-            return str(self.coefficient) + self.name
+    def __init__(self, name):
+        self.name = name
 
     def __add__(self, other):
-        if isinstance(other, Variable):
-            if self.name == other.name:
-                return Variable(str(self.coefficient + other.coefficient) + self.name)
-            else:
-                raise ValueError("Cannot add variables with different names")
-
-    def __sub__(self, other):
-        if isinstance(other, Variable):
-            if self.name == other.name:
-                return Variable(str(self.coefficient - other.coefficient) + self.name)
-            else:
-                raise ValueError("Cannot subtract variables with different names")
-
-    def __abs__(self):
-        return Variable(str(abs(self.coefficient)) + self.name)
+        if type(other) == Variable:
+            return Term([self, other])
+        elif type(other) == int:
+            return Term([self], other)
+        elif type(other) == Term:
+            return other * self
